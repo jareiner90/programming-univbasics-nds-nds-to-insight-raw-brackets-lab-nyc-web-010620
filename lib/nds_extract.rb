b414865a50b062ab82ed1f7c3a22c50fdeb5c258
John Reiner <johnreiner1990@gmail.com>
2020-01-03 13:03:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,7 +12,7 @@ def directors_totals(nds)
   while first_count < nds.count do
     second_count = 0
 
-    while second_count <= nds[first_count].count do
+    while second_count < nds.count do
       round_total = 0
       round_total += nds[first_count][:movies][second_count][:worldwide_gross]
       second_count += 1
