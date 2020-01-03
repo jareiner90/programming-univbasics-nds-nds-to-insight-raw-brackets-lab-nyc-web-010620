16ed44a0ef761f84a9797834fbfcd5496a8e03d7
John Reiner <johnreiner1990@gmail.com>
2020-01-03 12:58:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  pp nds[first_count][:movies][second_count][:worldwide_gross]
+  pp nds[0][:movies][0][:worldwide_gross]
   result = {
 
   }
