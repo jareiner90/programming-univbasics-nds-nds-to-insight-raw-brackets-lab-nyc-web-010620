60c65b568b140f68391fd4cf6ce1e84cd012287c
John Reiner <johnreiner1990@gmail.com>
2020-01-03 13:23:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,24 +2,22 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  pp nds[0][:movies][0][:worldwide_gross]
   result = {
 
   }
 
-  round_total = 0
+  
   first_count = 0
+
   while first_count < nds.count do
     second_count = 0
-
-    while second_count < nds.count do
-
-      round_total += nds[first_count][:movies][1][:worldwide_gross]
+    round_total = 0
+    while second_count < nds[first_count][:movies].count do
+      round_total += nds[first_count][:movies][second_count][:worldwide_gross]
       second_count += 1
     end
     result[nds[first_count][:name]] = round_total
     first_count += 1
-
   end
-  return result
+  pp result
 end
